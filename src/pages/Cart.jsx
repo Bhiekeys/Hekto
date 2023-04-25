278478d0edaf27a9f57c6a92d656dd8eadba1c65
Bhiekeys <tinuolajisodimu@gmail.com>
2023-04-25 21:27:11 +0100

    fix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,7 @@
 
 import Header from '../components/header/Header';
 import Header2 from '../components/header/Header2';
-import Cartbanner from '../components/carts/Cartbanner';
+import Cartbanner from '../components/carts/CartBanner';
 import Cartt from '../components/carts/Cartt';
 import Footer from '../components/footer/Footer';
 
